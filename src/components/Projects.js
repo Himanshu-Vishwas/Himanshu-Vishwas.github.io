72e2f915df98fabd790bb6ea6c30c3b1a7e2adee
Himanshu-vishwas <himanshuvishwas58@gmail.com>
2023-02-09 09:51:47 +0530

    added projects club website and akkubtech app

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,6 +4,8 @@ import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
 import anprojImg1 from "../assets/img/andp1.png";
+import projImg4 from "../assets/img/project-img4.png";
+
 import anprojImg2 from "../assets/img/andp2.png";
 import othrp1 from "../assets/img/othp1.png"
 import othrp2 from "../assets/img/othp2.png"
@@ -27,6 +29,11 @@ export const Projects = () => {
       title: "Product-preview-card-component",
       description: "A Frontend Mentor project",
       imgUrl: projImg3,
+    },
+    {
+      title: "bcefrontiers.live",
+      description: "A Full responsive multipage website for a college club",
+      imgUrl: projImg4,
     },
   ]
   
@@ -74,6 +81,28 @@ export const Projects = () => {
                   <h2>Projects</h2>
                   <p>
                     <ul align="left">
+                      
+                    <li>
+                      AKU Btech Syllabus Android App - 
+                        <a
+                          href="https://himanshu-vishwas.github.io/akubtech/"
+                          alt=""
+                          target="_blank"
+                        >
+                          Apk Link 
+                        </a>
+                      </li>
+                   
+                    <li>
+                    Website for a club -
+                        <a
+                          href="https://www.bcefrontiers.live"
+                          alt=""
+                          target="_blank"
+                        >
+                          bcefrontiers.live 
+                        </a>
+                      </li>
                       <li>
                         <a
                           href="https://developer.devgroup.engineer/bmi-calculator-react/"
